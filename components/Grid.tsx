f6640c9baaa49e1abc9a82906674f060b185a7e4
Anuj Singal <anujsingal203@gmail.com>
2026-04-05 22:02:09 +0530

    first  commit

---
+++ components/Grid.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+
+const Grid = () => {
+  return (
+    <section id="about" className="relative z-10 py-20 max-w-7xl mx-auto px-4">
+      <BentoGrid>
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            key={i}
+            id={item.id}
+            title={item.title}
+            description={item.description}
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+
+export default Grid;
