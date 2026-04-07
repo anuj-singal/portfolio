6f9eb6c6f74d818d57ae75618da1bc9f2c0c545b
Anuj Singal <anujsingal203@gmail.com>
2026-04-07 19:37:47 +0530

    bentogrid fixed

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { gridItems } from "@/data";
+import { gridItems } from "@/data/grid";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
   return (
-    <section id="about" className="relative z-10 py-20 max-w-7xl mx-auto px-4">
+    <section
+      id="about"
+      className="relative z-10 py-20 max-w-7xl mx-auto px-4"
+    >
       <BentoGrid>
         {gridItems.map((item, i) => (
           <BentoGridItem
---
+++ data/grid.ts
@@ -0,0 +1,46 @@
+export const gridItems = [
+  {
+    id: 1,
+    title: "I build scalable full-stack apps with clean architecture",
+    description: "Client-first mindset",
+    className:
+      "col-span-1 md:col-span-6 lg:col-span-3 md:row-span-3",
+    imgClassName: "w-full h-full object-cover",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+  },
+
+  {
+    id: 3,
+    title: "Tech Stack",
+    description: "Constantly evolving",
+    className:
+      "col-span-1 md:col-span-3 lg:col-span-2 md:row-span-2",
+  },
+
+  {
+    id: 2,
+    title: "Flexible across time zones 🌍",
+    description: "Always available",
+    className:
+      "col-span-1 md:col-span-3 lg:col-span-2 md:row-span-3",
+  },
+
+  {
+    id: 6,
+    title: "Copy my email & let’s connect",
+    description: "Let's build something impactfull together",
+    className:
+      "col-span-1 md:col-span-6 lg:col-span-2 md:row-span-2",
+  },
+
+  {
+    id: 4,
+    title: "Passionate developer ⚡",
+    description: "I love building things",
+    className:
+      "col-span-1 md:col-span-3 lg:col-span-1 md:row-span-2",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+];
